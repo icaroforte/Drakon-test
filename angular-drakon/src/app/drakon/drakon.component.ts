6732a364347ec6a15f0495b1d76c60428f9c8b27
Ícaro <icarofort@gmail.com>
2018-10-13 15:54:55 -0300

    update layout

--- angular-drakon/src/app/drakon/drakon.component.ts
+++ angular-drakon/src/app/drakon/drakon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
 import { DataService } from '../data.service';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-drakon',
